186d14cb07959f6a3cd4ca058654ce7af3479f56
Elliot Ritchie <elliotritchie@hotmail.com>
2018-10-10 10:56:18 +0100

    Added structure map registries

---
+++ src/SFA.DAS.Authorization.EmployerRoles/EmployerRolesAuthorizationRegistry.cs
@@ -0,0 +1,12 @@
+﻿using StructureMap;
+
+namespace SFA.DAS.Authorization.EmployerRoles
+{
+    public class EmployerRolesAuthorizationRegistry : Registry
+    {
+        public EmployerRolesAuthorizationRegistry()
+        {
+            For<IAuthorizationHandler>().Add<EmployerRolesAuthorizationHandler>();
+        }
+    }
+}
---
+++ src/SFA.DAS.Authorization.ProviderPermissions/ProviderPermissionsAuthorizationRegistry.cs
@@ -0,0 +1,12 @@
+﻿using StructureMap;
+
+namespace SFA.DAS.Authorization.ProviderPermissions
+{
+    public class ProviderPermissionsAuthorizationRegistry : Registry
+    {
+        public ProviderPermissionsAuthorizationRegistry()
+        {
+            For<IAuthorizationHandler>().Add<ProviderPermissionsAuthorizationHandler>();
+        }
+    }
+}
