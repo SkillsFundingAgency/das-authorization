48fee0f9816dc69dfc9165790781039d603db882
Elliot Ritchie <elliotritchie@hotmail.com>
2018-10-12 15:46:22 +0100

    Added employer roles any option

--- src/SFA.DAS.Authorization.EmployerRoles/EmployerRoles.cs
+++ src/SFA.DAS.Authorization.EmployerRoles/EmployerRoles.cs
@@ -2,6 +2,7 @@
 {
     public struct EmployerRoles
     {
+        public const string Any = "EmployerRoles.Any";
         public const string Owner = "EmployerRoles.Owner";
         public const string Transactor = "EmployerRoles.Transactor";
         public const string Viewer = "EmployerRoles.Viewer";
