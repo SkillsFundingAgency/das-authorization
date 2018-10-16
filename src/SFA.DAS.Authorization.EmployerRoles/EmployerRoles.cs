2655a3050c363098f019f90e5216713d722a29db
Elliot Ritchie <elliotritchie@hotmail.com>
2018-10-16 09:43:49 +0100

    Added owner or transactor role

--- src/SFA.DAS.Authorization.EmployerRoles/EmployerRoles.cs
+++ src/SFA.DAS.Authorization.EmployerRoles/EmployerRoles.cs
@@ -4,6 +4,7 @@
     {
         public const string Any = "EmployerRoles.Any";
         public const string Owner = "EmployerRoles.Owner";
+        public const string OwnerOrTransactor = Owner + "," + Transactor;
         public const string Transactor = "EmployerRoles.Transactor";
         public const string Viewer = "EmployerRoles.Viewer";
     }
