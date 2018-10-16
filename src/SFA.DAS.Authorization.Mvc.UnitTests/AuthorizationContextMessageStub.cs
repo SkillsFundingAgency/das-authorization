5e7faf450807abcf907957a037af7824f46b7382
Elliot Ritchie <elliotritchie@hotmail.com>
2018-10-16 11:16:17 +0100

    Refactored csproj files

--- src/SFA.DAS.Authorization.Mvc.UnitTests/AuthorizationContextMessageStub.cs
+++ src/SFA.DAS.Authorization.Mvc.UnitTests/AuthorizationContextMessageStub.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
 
 namespace SFA.DAS.Authorization.Mvc.UnitTests
 {
     public class AuthorizationContextMessageStub : IAuthorizationContextMessage
     {
-        [Required]
         public Guid? UserRef { get; set; }
     }
 }
--- src/SFA.DAS.Authorization.WebApi.UnitTests/AuthorizationContextMessageStub.cs
+++ src/SFA.DAS.Authorization.WebApi.UnitTests/AuthorizationContextMessageStub.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
 
 namespace SFA.DAS.Authorization.WebApi.UnitTests
 {
     public class AuthorizationContextMessageStub : IAuthorizationContextMessage
     {
-        [Required]
         public string Foo { get; set; }
-
-        [Required]
         public Guid? UserRef { get; set; }
     }
 }
