fb5d7015041bbaa6c10ef95dac98c95a5a239fb8
tom-gough <tom@tgsd.co.uk>
2018-12-21 09:52:06 +0000

    Tidied up test harness

--- src/WebApplication3/Controllers/HomeController.cs
+++ src/WebApplication3/Controllers/HomeController.cs
@@ -1,32 +1,95 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
 using SFA.DAS.Authorization.Mvc;
+using WebApplication3.Models;
 
 namespace WebApplication3.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+
+        public HomeController()
+        {
+        }
+
+        public HomeController(ApplicationSignInManager signInManager, ApplicationUserManager userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public ApplicationSignInManager SignInManager {
+            get {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+            private set {
+                _signInManager = value;
+            }
+        }
+
+        public ApplicationUserManager UserManager {
+            get {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set {
+                _userManager = value;
+            }
+        }
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        [DasAuthorize(TestOptions.DasAuthorizeTrue)]
+        public ActionResult DasAuthorizeTrue()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "DasAuthorizeTrue";
 
             return View();
         }
 
         [DasAuthorize(TestOptions.DasAuthorizeFalse)]
-        public ActionResult Contact()
+        public ActionResult DasAuthorizeFalse()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "DasAuthorizeFalse";
 
             return View();
         }
+
+        [DasAuthorize(TestOptions.DasAuthorizeFalseWithErrors)]
+        public ActionResult DasAuthorizeFalseWithErrors()
+        {
+            ViewBag.Message = "DasAuthorizeFalseWithErrors";
+
+            return View();
+        }
+
+        public async Task<ActionResult> AutoLogin()
+        {
+            const string username = "test@example.com";
+            var user = UserManager.Users.FirstOrDefault(x => x.UserName == username);
+            if (user == null)
+            {
+                user = new ApplicationUser { UserName = username, Email = username };
+                var result = await UserManager.CreateAsync(user, "Test11!!");
+                if (!result.Succeeded)
+                {
+                    throw new Exception("Error automatically registering a user in test harness");
+                }
+            }
+
+            await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
+            return RedirectToAction("Index", "Home");
+        }
+
     }
 }
