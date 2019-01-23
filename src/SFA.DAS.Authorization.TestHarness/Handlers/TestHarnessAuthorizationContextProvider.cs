90b4e6549026b9de61c88ced73d9034c4197d6fa
Neil Winstanley <Winsto@users.noreply.github.com>
2019-01-23 14:07:23 +0000

    UserRef will now need to be a specific value, so as to be consistent with that in ReadStore

--- src/SFA.DAS.Authorization.TestHarness/Handlers/TestHarnessAuthorizationContextProvider.cs
+++ src/SFA.DAS.Authorization.TestHarness/Handlers/TestHarnessAuthorizationContextProvider.cs
@@ -9,7 +9,7 @@ namespace SFA.DAS.Authorization.TestHarness.Handlers
             var context = new AuthorizationContext();
             context.Set("AccountId", (long?)112);
             context.Set("UserEmail", "test");
-            context.Set("UserRef", (Guid?)Guid.NewGuid());
+            context.Set("UserRef", (Guid?) new Guid("121AF597-42D1-4AEE-96BE-D1F88293B140"));
             context.Set("Ukprn", (long?)382938712);
             context.Set("AccountLegalEntityId", (long?)114);
             return context;
