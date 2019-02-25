e1cde3a527223c320f41852e9df41b7f1dec14a9
Elliot Ritchie <elliotritchie@hotmail.com>
2019-02-25 10:08:24 +0000

    Updated TestOption.Prefix to include period

--- src/SFA.DAS.Authorization.NetCoreTestHarness/Authorization/TestOption.cs
+++ src/SFA.DAS.Authorization.NetCoreTestHarness/Authorization/TestOption.cs
@@ -2,13 +2,13 @@
 {
     public static class TestOption
     {
-        public const string Prefix = "TestOption";
+        public const string Prefix = "TestOption.";
         public const string AuthorizedOption = "Authorized";
         public const string UnauthorizedSingleErrorOption = "UnauthorizedSingleError";
         public const string UnauthorizedMultipleErrorsOptions = "UnauthorizedMultipleErrors";
         
-        public const string Authorized = Prefix + "." + AuthorizedOption;
-        public const string UnauthorizedSingleError = Prefix + "." + UnauthorizedSingleErrorOption;
-        public const string UnauthorizedMultipleErrors = Prefix + "." + UnauthorizedMultipleErrorsOptions;
+        public const string Authorized = Prefix + AuthorizedOption;
+        public const string UnauthorizedSingleError = Prefix + UnauthorizedSingleErrorOption;
+        public const string UnauthorizedMultipleErrors = Prefix + UnauthorizedMultipleErrorsOptions;
     }
 }
