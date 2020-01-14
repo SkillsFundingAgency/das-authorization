7bb5a7fccac4cfca2202470a7cd2e18fc834b7cf
Kevin <kevin@stephek.co.uk>
2020-01-14 09:41:11 +0000

    CON-1016-fix-default-DI-issue

--- src/SFA.DAS.Authorization.ProviderPermissions/DependencyResolution/StructureMap/ProviderPermissionsAuthorizationRegistry.cs
+++ src/SFA.DAS.Authorization.ProviderPermissions/DependencyResolution/StructureMap/ProviderPermissionsAuthorizationRegistry.cs
@@ -1,5 +1,7 @@
-﻿using SFA.DAS.Authorization.DependencyResolution.StructureMap;
+﻿using SFA.DAS.Authorization.Caching;
+using SFA.DAS.Authorization.DependencyResolution.StructureMap;
 using SFA.DAS.Authorization.Handlers;
+using SFA.DAS.Authorization.ProviderPermissions.Context;
 using SFA.DAS.Authorization.ProviderPermissions.Handlers;
 using SFA.DAS.ProviderRelationships.Api.Client.DependencyResolution.StructureMap;
 using StructureMap;
@@ -10,6 +12,7 @@ namespace SFA.DAS.Authorization.ProviderPermissions.DependencyResolution.Structu
     {
         public ProviderPermissionsAuthorizationRegistry()
         {
+            For<IAuthorizationResultCacheConfigurationProvider>().Add<AuthorizationResultCacheConfigurationProvider>();
             For<IAuthorizationHandler>().Add<AuthorizationHandler>().InterceptWith(new AuthorizationResultLoggerInterceptor());
             IncludeRegistry<ProviderRelationshipsApiClientRegistry>();
         }
