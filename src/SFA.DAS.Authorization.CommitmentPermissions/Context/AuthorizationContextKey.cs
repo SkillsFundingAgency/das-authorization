f6b886990bca8d00beba531c87a93a330f7bdb1e
Shahzad <shahzad.khan@digital.education.gov.uk>
2020-01-20 16:49:46 +0000

    CV-571 Removed Typo.

--- src/SFA.DAS.Authorization.CommitmentPermissions/Context/AuthorizationContextKey.cs
+++ src/SFA.DAS.Authorization.CommitmentPermissions/Context/AuthorizationContextKey.cs
@@ -3,7 +3,7 @@
     public static class AuthorizationContextKey
     {
         public const string CohortId = "CohortId";
-        public const string ApprenticeshipId = "ApprenticeshiptId";
+        public const string ApprenticeshipId = "ApprenticeshipId";
         public const string PartyId = "PartyId";
         public const string Party = "Party";
     }
