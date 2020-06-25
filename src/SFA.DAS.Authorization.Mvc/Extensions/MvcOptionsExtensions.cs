ae3d2e74236efa71209105c905853ee19c74613a
chrisfoster186 <chrisfoster186@googlemail.com>
2020-06-25 08:14:32 +0100

    Sets index for AuthModelBinder to maxvalue

--- src/SFA.DAS.Authorization.Mvc/Extensions/MvcOptionsExtensions.cs
+++ src/SFA.DAS.Authorization.Mvc/Extensions/MvcOptionsExtensions.cs
@@ -10,7 +10,7 @@ namespace SFA.DAS.Authorization.Mvc.Extensions
         public static void AddAuthorization(this MvcOptions mvcOptions)
         {
             mvcOptions.Filters.Add<AuthorizationFilter>(int.MaxValue);
-            mvcOptions.ModelBinderProviders.Insert(0, new AuthorizationModelBinderProvider());
+            mvcOptions.ModelBinderProviders.Insert(int.MaxValue, new AuthorizationModelBinderProvider());
         }
     }
 }
