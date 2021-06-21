5b0ea1005a0d8184c9b0db3b96d906329d9d6401
Paul Howes <paul.howes@cognetics.net>
2021-06-21 12:05:08 +0100

    Update CommitmentPermissionsApiClientConfiguration.cs

--- src/SFA.DAS.Authorization.CommitmentPermissions/Configuration/CommitmentPermissionsApiClientConfiguration.cs
+++ src/SFA.DAS.Authorization.CommitmentPermissions/Configuration/CommitmentPermissionsApiClientConfiguration.cs
@@ -5,9 +5,6 @@ namespace SFA.DAS.Authorization.CommitmentPermissions.Configuration
     public class CommitmentPermissionsApiClientConfiguration : IManagedIdentityClientConfiguration
     {
         public string ApiBaseUrl { get; set; }
-        public string Tenant { get; set; }
-        public string ClientId { get; set; }
-        public string ClientSecret { get; set; }
         public string IdentifierUri { get; set; }
     }
 }
